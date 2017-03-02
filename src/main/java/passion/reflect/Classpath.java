53fbc3c4cf63937662eba0466b7ba5f3ff603686
PizzaCrust <pizzacrustcoder@gmail.com>
2017-03-02 15:53:38 +0700

    Defining classes

--- src/main/java/passion/reflect/Classpath.java
+++ src/main/java/passion/reflect/Classpath.java
@@ -4,6 +4,7 @@ import sun.misc.IOUtils;
 
 import java.io.File;
 import java.io.IOException;
+import java.lang.reflect.Method;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.Enumeration;
@@ -29,6 +30,21 @@ public class Classpath {
 
     public static Classpath from(ClassLoader classLoader) {
         return new Classpath(classLoader);
+    }
+
+    /**
+     * Loads a JVM class into the classpath.
+     * This does not replace classes, if you do so! It will cause a {@link LinkageError}.
+     * @param jvmClass
+     * @throws Exception
+     */
+    public void loadClass(JvmClass jvmClass) throws Exception {
+        Method defineClassMethod = classLoader.getClass().getDeclaredMethod("defineClass", String
+                .class, byte[].class, int.class, int.class);
+        String name = jvmClass.getName().reflect();
+        defineClassMethod.setAccessible(true);
+        defineClassMethod.invoke(classLoader, name, jvmClass.toBytes(), 0, jvmClass.toBytes()
+                .length);
     }
 
     /**
